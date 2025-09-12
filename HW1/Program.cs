358b4705c4fe342ac18001cc09df22a11585760f
ttu1611 <ttu55330@gmail.com>
2025-09-12 07:45:53 +0700

    Cập nhật HW1/Program.cs

--- HW1/Program.cs
+++ HW1/Program.cs
@@ -1,16 +1,28 @@
 ﻿using System;
 
+
 class Program
 {
     static void Main()
     {
-        Console.OutputEncoding = System.Text.Encoding.UTF8;
-        Console.InputEncoding = System.Text.Encoding.UTF8;
-        string hoTen = "Trần Đức Tuấn Tú";
-        string maSV = "12424033";
-        string lop = "12424TN";
-        string github = "https://github.com/ttu1611/oop";
-        string email = "ttu55330@gmail.com";
+        string ten, masv, lop, email, github;
+
+        Console.Writeline("Nhập họ và tên:");
+        ten = Console.ReadLine();
+
+        Console.WriteLine("Mã sinh viên:");
+        masv = Console.ReadLine();
+
+        Console.WriteLine("Lớp:");
+        lop = Console.ReadLine();
+
+        Console.WriteLine("Email:");
+        email = Console.ReadLine();
+       
+
+        Console.WriteLine("Link github:");
+        github = Console.ReadLine();
+
         Console.WriteLine("{0}\t{1}\t{2}\t{3}\t{4}", hoTen, maSV, lop, github, email);
     }
 }
